2abf2a587a93d341450c2b0e980416b0af72f133
Orlando Del Aguila <orlando@hashlabs.com>
2019-08-31 11:29:46 -0500

    dev(fundraising): setup postgres credentials

--- live/stage/data-stores/postgres/setup/fundraising.tf
+++ live/stage/data-stores/postgres/setup/fundraising.tf
@@ -1,22 +1,22 @@
-resource "random_string" "username" {
+resource "random_string" "fundraising_db_user" {
   length           = 8
   special          = true
   override_special = "/@\" "
 }
 
-resource "random_password" "password" {
+resource "random_password" "fundraising_db_pass" {
   length           = 16
   special          = true
   override_special = "/@\" "
 }
 
 resource "postgresql_role" "fundraising" {
-  name     = "fundraising_${random_string.username.result}"
+  name     = "fundraising_${random_string.fundraising_db_user.result}"
   login    = true
-  password = random_password.password.result
+  password = random_password.fundraising_db_pass.result
 }
 
 resource "postgresql_database" "fundraising" {
   name  = "fundraising_${local.environment}"
-  owner = "fundraising_${random_string.username.result}"
+  owner = "fundraising_${random_string.fundraising_db_user.result}"
 }
---
+++ live/stage/data-stores/postgres/setup/outputs.tf
@@ -0,0 +1,7 @@
+output "fundraising_db_user" {
+  value = random_string.fundraising_db_user.result
+}
+
+output "fundraising_db_pass" {
+  value = random_password.fundraising_db_pass.result
+}
