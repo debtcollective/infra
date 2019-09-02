160e02b9415ab877047cf1b2485444271d7377b6
Orlando Del Aguila <orlando@hashlabs.com>
2019-09-02 15:47:36 -0500

    dev(fundraising): move container definitions to a separated file

--- modules/services/fundraising/main.tf
+++ modules/services/fundraising/main.tf
@@ -117,50 +117,6 @@ module "autoscaling" {
   subnet_ids              = var.subnet_ids
 }
 
-resource "aws_cloudwatch_log_group" "fundraising" {
-  name = "${var.environment}-fundraising-lg"
-
-  tags = {
-    Environment = var.environment
-    Application = "fundraising"
-    Terraform   = true
-  }
-}
-
-module "container_definitions" {
-  source = "git::https://github.com/cloudposse/terraform-aws-ecs-container-definition.git?ref=0.15.0"
-
-  container_name               = local.container_name
-  container_cpu                = 0
-  container_memory             = 0
-  container_memory_reservation = 479
-  essential                    = true
-  container_image              = var.container_image
-
-  environment = [
-    {
-      name  = "RAILS_ENV",
-      value = "staging"
-    },
-    {
-      name  = "DATABASE_URL",
-      value = var.database_url
-    }
-  ]
-
-  port_mappings = [
-    {
-      containerPort = local.container_port
-    }
-  ]
-
-  log_driver = "awslogs"
-  log_options = {
-    "awslogs-region" = data.aws_region.current.name
-    "awslogs-group"  = aws_cloudwatch_log_group.fundraising.name
-  }
-}
-
 // Create ECS cluster
 resource "aws_ecs_cluster" "fundraising" {
   name = "fundraising-${var.environment}"
