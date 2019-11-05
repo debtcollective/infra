679a8561407de25c0129f220e67eb0f0cf4fba80
Orlando Del Aguila <orlando@hashlabs.com>
2019-11-05 16:00:59 -0600

    dev(mailforgood): output postgres setup variables

--- live/stage/data-stores/postgres/setup/outputs.tf
+++ live/stage/data-stores/postgres/setup/outputs.tf
@@ -23,3 +23,15 @@ output "disputes_api_db_name" {
   value = "disputes_api_${local.environment}"
 
 }
+
+output "mail_for_good_db_user_ssm_key" {
+  value = aws_ssm_parameter.mail_for_good_db_user.name
+}
+
+output "mail_for_good_db_pass_ssm_key" {
+  value = aws_ssm_parameter.mail_for_good_db_pass.name
+}
+
+output "mail_for_good_db_name" {
+  value = postgresql_database.mail_for_good.name
+}
