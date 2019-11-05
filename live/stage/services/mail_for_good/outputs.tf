b3c3062d22d8cf7370e47f922d43903764e18aa8
Orlando Del Aguila <orlando@hashlabs.com>
2019-11-05 13:12:43 -0600

    dev(mailforgood): fix outputs references

--- live/stage/services/mail_for_good/outputs.tf
+++ live/stage/services/mail_for_good/outputs.tf
@@ -1,14 +1,14 @@
 output "service_name" {
-  value       = "${module.fundraising.service_name}"
+  value       = module.mail_for_good.service_name
   description = "ECS Service name"
 }
 
 output "dns_name" {
-  value       = "${module.fundraising.dns_name}"
+  value       = module.mail_for_good.dns_name
   description = "ELB dns_name"
 }
 
 output "zone_id" {
-  value       = "${module.fundraising.zone_id}"
+  value       = module.mail_for_good.zone_id
   description = "ELB zone_id"
 }
