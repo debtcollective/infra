5475dd7154d8ceaf532ee4a4d936830e9f0bee7f
Orlando Del Aguila <orlando@hashlabs.com>
2019-11-11 21:12:46 -0600

    dev(cluster): move autoscaling to cluster

--- live/stage/services/cluster/dependencies.tf
+++ live/stage/services/cluster/dependencies.tf
@@ -1,3 +1,15 @@
+data "terraform_remote_state" "iam" {
+  backend = "remote"
+
+  config = {
+    organization = local.remote_state_organization
+
+    workspaces = {
+      name = local.iam_remote_state_workspace
+    }
+  }
+}
+
 data "terraform_remote_state" "vpc" {
   backend = "remote"
 
@@ -11,9 +23,18 @@ data "terraform_remote_state" "vpc" {
 }
 
 locals {
-  remote_state_organization  = "debtcollective"
-  vpc_remote_state_workspace = "stage-network"
+  environment = "stage"
 
-  security_group_ids = [data.terraform_remote_state.vpc.outputs.elb_security_group_id]
-  subnet_ids         = data.terraform_remote_state.vpc.outputs.public_subnet_ids
+  ec2_security_group_ids  = [data.terraform_remote_state.vpc.outputs.ec2_security_group_id]
+  elb_security_group_ids  = [data.terraform_remote_state.vpc.outputs.elb_security_group_id]
+  iam_instance_profile_id = data.terraform_remote_state.iam.outputs.instance_profile_id
+  instance_type           = "t3a.nano"
+  key_name                = data.terraform_remote_state.vpc.outputs.ssh_key_pair_name
+  subnet_ids              = data.terraform_remote_state.vpc.outputs.public_subnet_ids
+  vpc_id                  = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  iam_remote_state_workspace = "global-iam"
+  remote_state_organization  = "debtcollective"
+  vpc_remote_state_workspace = "${local.environment}-network"
+
 }
