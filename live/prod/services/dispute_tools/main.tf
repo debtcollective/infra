1e44915eca81e72f18d2a6abfddbc2f9fefba696
Orlando Del Aguila <orlando@hashlabs.com>
2020-02-04 03:05:54 -0600

    dev(prod): configure dispute_tools module

--- live/prod/services/dispute_tools/main.tf
+++ live/prod/services/dispute_tools/main.tf
@@ -21,7 +21,7 @@ data "aws_route53_zone" "primary" {
 
 resource "aws_route53_record" "dispute_tools" {
   zone_id = data.aws_route53_zone.primary.zone_id
-  name    = "dispute-tools-infra"
+  name    = "tools-prod"
   type    = "A"
 
   alias {
