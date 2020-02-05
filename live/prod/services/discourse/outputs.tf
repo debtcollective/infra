6ef79da9b37e8e6850fcc1e2145b0e192d8a8b89
Orlando Del Aguila <orlando@hashlabs.com>
2020-02-04 20:28:33 -0600

    dev(prod): output discourse domain

--- live/prod/services/discourse/outputs.tf
+++ live/prod/services/discourse/outputs.tf
@@ -2,3 +2,8 @@ output "public_ip" {
   value       = module.discourse.public_ip
   description = "Instance public ip"
 }
+
+output "domain" {
+  value       = aws_route53_record.discourse.fqdn
+  description = "Domain name"
+}
