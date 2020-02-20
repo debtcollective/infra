5d5ad6b0c7d6af127b675f6e256c27e1b24a72eb
Orlando Del Aguila <orlando@hashlabs.com>
2020-02-19 18:42:47 -0600

    dev(discourse): allow cloudfront to read s3

--- live/prod/services/discourse/s3.tf
+++ live/prod/services/discourse/s3.tf
@@ -103,6 +103,37 @@ data "aws_iam_policy_document" "uploads" {
       identifiers = ["*"]
     }
   }
+
+  // Enable access from Cloudfront Distribution
+  statement {
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.uploads.arn
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.uploads.iam_arn]
+    }
+  }
+
+  statement {
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.uploads.arn}/*"
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.uploads.iam_arn]
+    }
+  }
 }
 
 resource "aws_s3_bucket_policy" "uploads" {
