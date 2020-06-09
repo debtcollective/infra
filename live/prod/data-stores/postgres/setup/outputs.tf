c29470e556a13b2b4620786261897c14a5017c1b
Orlando Del Aguila <orlando@hashlabs.com>
2020-06-09 12:32:41 -0500

    dev: remove ghost database from postgres, since ghost requires mysql as db

--- live/prod/data-stores/postgres/setup/outputs.tf
+++ live/prod/data-stores/postgres/setup/outputs.tf
@@ -81,16 +81,3 @@ output "campaign_api_db_name" {
   value = "campaign_api_${local.environment}"
 
 }
-
-# Ghost
-output "ghost_db_user_ssm_key" {
-  value = aws_ssm_parameter.ghost_db_user.name
-}
-
-output "ghost_db_pass_ssm_key" {
-  value = aws_ssm_parameter.ghost_db_pass.name
-}
-
-output "ghost_db_name" {
-  value = "ghost_${local.environment}"
-}
