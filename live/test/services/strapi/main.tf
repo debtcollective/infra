a89010aa4889a59c6a88b0ae906dd68e350aaf14
Orlando Del Aguila <orlando@hashlabs.com>
2021-03-16 20:00:03 -0600

    dev(strapi): add strapi test service

---
+++ live/test/services/strapi/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_version = ">=0.12.20"
+
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "debtcollective"
+
+    workspaces {
+      name = "test-services-strapi"
+    }
+  }
+}
+
+provider "aws" {
+  version = "~> 2.0"
+}
+
+data "aws_route53_zone" "primary" {
+  name = "debtcollective.org"
+}
+
+resource "aws_route53_record" "strapi" {
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = "strapi-${local.environment}"
+  type    = "A"
+
+  alias {
+    name                   = local.lb_dns_name
+    zone_id                = local.lb_zone_id
+    evaluate_target_health = false
+  }
+}
+
+module "strapi" {
+  source      = "../../../../modules/services/strapi"
+  environment = local.environment
+
+  domain                       = aws_route53_record.strapi.fqdn
+  ecs_cluster_id               = local.ecs_cluster_id
+  lb_listener_id               = local.lb_listener_id
+  vpc_id                       = local.vpc_id
+  container_memory_reservation = 256
+
+  db_name     = local.db_name
+  db_host     = local.db_host
+  db_port     = local.db_port
+  db_username = local.db_username
+  db_password = local.db_password
+}
