0ff665bdef2b345016ca2ae04c6a44d21728fe42
Paul Lanctot <paul.e.lanctot@protonmail.com>
2022-11-10 11:42:57 -0800

    Update vars.tf

--- modules/services/chatwoot/vars.tf
+++ modules/services/chatwoot/vars.tf
@@ -5,7 +5,7 @@ variable "environment" {
 
 variable "container_image" {
   description = "Docker image name"
-  default     = "chatwoot/chatwoot:v2.8.1-ce"
+  default     = "chatwoot/chatwoot:latest"
 }
 
 variable "container_memory_reservation" {
