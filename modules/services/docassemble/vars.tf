696be81b98671f2ca2fdf550a72c83b152c1d061
Paul Lanctot <paul.e.lanctot@protonmail.com>
2022-12-14 00:10:54 -0800

    fix: switch to latest docassemble

--- modules/services/docassemble/vars.tf
+++ modules/services/docassemble/vars.tf
@@ -4,7 +4,7 @@ variable "environment" {
 
 variable "container_image" {
   description = "Docker image name"
-  default     = "hissingpanda/docassemble:1.4.8"
+  default     = "jhpyle/docassemble:latest"
 }
 
 variable "container_memory_reservation" {
