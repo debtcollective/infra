4132d4463aaecd558cc1371bc885a085396a478d
Paul Lanctot <7660115+hissingpanda@users.noreply.github.com>
2023-05-18 23:48:58 -0700

    feat: downgrade size of test ec2 instances

--- modules/utils/autoscaling/user_data.sh
+++ modules/utils/autoscaling/user_data.sh
@@ -8,9 +8,9 @@ sudo sed -i 's/#Port 22$/Port 12345/' /etc/ssh/sshd_config
 sudo service sshd restart || sudo service ssh restart
 
 # Tools
-sudo yum install htop -y
-sudo yum install rsync -y
-sudo yum install nfs-utils -y
+# sudo yum install htop -y
+# sudo yum install rsync -y
+# sudo yum install nfs-utils -y
 
 # Create swap file
 sudo fallocate -l 1G /swapfile
@@ -24,4 +24,4 @@ echo 'vm.vfs_cache_pressure=50' | sudo tee -a /etc/sysctl.conf
 
 # Mount EFS
 sudo mkdir -p /mnt/efs/wordpress
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_dns_name}:/ /mnt/efs/wordpress
+# sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_dns_name}:/ /mnt/efs/wordpress
