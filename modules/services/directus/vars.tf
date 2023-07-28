1d4015c3fa9d29f510e1357961f82b02ebb8e880
Paul Lanctot <7660115+hissingpanda@users.noreply.github.com>
2023-07-28 11:59:13 -0700

    fix: set directus version to 10.1.0

--- modules/services/directus/vars.tf
+++ modules/services/directus/vars.tf
@@ -4,7 +4,7 @@ variable "environment" {
 
 variable "container_image" {
   description = "Docker image name"
-  default     = "directus/directus:latest"
+  default     = "directus/directus:10.1.0"
 }
 
 variable "container_memory_reservation" {
